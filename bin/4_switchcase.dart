ffee3841d19cb984a6f23ff2f5a114277dfbc219
toomtarm007 <63122660102@g.lpru.ac.th>
2022-01-07 15:39:15 +0700

    1st

---
+++ bin/4_switchcase.dart
@@ -0,0 +1,23 @@
+// การเรียนรู้ภาษา Dart
+// หัวข้อที่ 6 เรื่อง Conditon swith...case
+
+void main(List<String> args) {
+  String grade = 'B';
+
+  switch (grade) {
+    case 'A':
+      print("Excellent grade of A");
+      break;
+    case 'B':
+      print("Very good !");
+      break;
+    case 'C':
+      print("Good enough. but work hard");
+      break;
+    case 'F':
+      print('You have failed');
+      break;
+    default:
+      print('Invalid Grade');
+  }
+}
